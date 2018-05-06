b208335a7d3ce2e30394230bb455bd5f95cde86b
quentinmrzt <quentin.morizot@outlook.fr>
2018-05-06 09:12:58 +0200

    Génération de la map, affichage et clique

--- src/jeu/squareAssembler.py
+++ src/jeu/squareAssembler.py
@@ -1,6 +1,18 @@
 #-*-coding: Latin-1-*-
 from tkinter import *
 from tkinter.messagebox import showinfo
+import random
+
+
+
+def test(event):
+    global tailleCarre, listeCouleur, l_map
+    x = (int)(event.x/tailleCarre)
+    y = (int)(event.y/tailleCarre)
+    print("[",x,"/",y,"]")
+    print("Couleur: ",listeCouleur[l_map[y][x]])
+    
+    
 
 
 root = Tk()
@@ -30,5 +42,51 @@ menubar.add_cascade(label="A propos", menu=editmenu)
 
 # affichage du menu
 root.config(menu=menubar)
+
+
+# Generation de la map
+tailleX = 20; tailleY = 20
+nbCouleur = 8
+nbParCouleur = [int((tailleX*tailleY)/nbCouleur)]*nbCouleur
+listeCouleur = ["red","blue","yellow","green","cyan","magenta","violet","teal"]
+
+l_map = [[0]*tailleX for i in range(tailleY)]
+x = 0
+while x<tailleX:
+    y = 0
+    while y<tailleY:
+        trouve = False
+        while(not trouve):
+            rdm =  random.randint(0,nbCouleur-1)
+            if(nbParCouleur[rdm]!=0):
+                nbParCouleur[rdm] = nbParCouleur[rdm]-1
+                l_map[y][x] = rdm
+                trouve = True
+        y = y+1
+    x = x+1
+
+
+# Affichage de la map
+tailleCarre = 20
+canvas = Canvas(root, width=tailleCarre*tailleX, height=tailleCarre*tailleY, background='grey')
+
+x = 0
+while x<tailleX:
+    y = 0
+    while y<tailleY: 
+        image = canvas.create_rectangle(x*tailleCarre,y*tailleCarre,x*tailleCarre+tailleCarre,y*tailleCarre+tailleCarre,fill=listeCouleur[l_map[y][x]])
+        y = y+1
+    x = x+1
+
+
+
+canvas.bind('<Button-1>', test)
+
+
+
+#canvas.coords(image, 0, 0, 5, 5)
+canvas.pack()
+
+
 
 root.mainloop()
